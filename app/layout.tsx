f6db23a77ffd917ba0790132779c49a04ad463b7
iamabdirazak <a.razakmubarak@icloud.com>
2025-12-01 10:57:39 +0300

    add navigation to the landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-gray text-white">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl font-bold mb-6">Welcome to the Notes App</h1>
+            <a
+              href="/notes"
+              className="inline-block px-6 py-3 bg-white text-black rounded-lg hover:bg-white-300"
+            >
+              Go to Notes
+            </a>
+          </div>;
 }
